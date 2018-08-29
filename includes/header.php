0546e4b649395412275e388dc46e6f64c35bcdd2
maricaadrian <marica.adrian@yahoo.com>
2018-08-29 19:27:33 +0300

    Startup version

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>
+<?php
+    if(isset($title))
+    {
+        echo $title;
+    }
+    else
+    {
+        echo "No Title";
+    }
+?>
+</title>
+
+
+<link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css">
